9c173b0fc233815f9f2ca5cc1f6efc1189fafef8
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2026-08-24 13:53:20 -0700

    Add: pascals-triangle - submission-3

---
+++ Data Structures & Algorithms/pascals-triangle/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        ans = []
+
+        for row in range(numRows):
+            length = row + 1
+
+            row_create = [1 for i in range(length)]
+
+            for i in range(1, len(row_create) - 1):
+                left = ans[row - 1][i - 1] 
+                right = ans[row - 1][i] 
+                
+                # print("____" + str(ans))
+                print(f" (row: {row}, i : {i}): {left} , {right}")
+                
+                row_create[i] = left + right
+                print(row_create)
+
+
+            
+
+            ans.append(row_create)
+
+            
+        
+        return ans 
+        
