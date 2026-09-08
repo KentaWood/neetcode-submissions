1e6999fb19f484adb735436621adc0ff2b2cc51b
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2026-09-08 14:48:51 -0700

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        mapping = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz",
+            }
+
+        ans = []
+
+        def bt(curr: str, combo: str) -> None:
+            print("here")
+    
+            if len(combo) == len(digits):
+                
+                if combo != "":
+                    ans.append(combo)
+                return
+
+            p = len(curr)
+            # print(f"curr: {curr}  pointer: {p}")
+            
+            for let in mapping[digits[p]]:
+                # print(curr + digits[p], combo + let)
+                bt(curr + digits[p], combo + let)
+                
+            
+            
+
+        bt("","")
+
+        return ans
+
+        
